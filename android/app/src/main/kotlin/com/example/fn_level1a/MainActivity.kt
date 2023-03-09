9ad9550521e082f63c41b1f9f7552dabe0972e8b
Harry Lesmana <harrysl21@gmail.com>
2023-03-09 16:08:46 +0700

    First Commit

---
+++ android/app/src/main/kotlin/com/example/fn_level1a/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fn_level1a
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text("Flutter"),
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image(
+                image: AssetImage('images/flutterlogo.png'),
+              ),
+              Text(
+                "belajar flutter".toUpperCase(),
+                style: TextStyle(fontSize: 30, color: Colors.white),
+              ),
+              Text(
+                "ARN222-22062".toUpperCase(),
+                style: TextStyle(fontSize: 18, color: Colors.white),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
+}
